45d9c9c1183f4da5f7bcf7440f1cc65256f3d2b7
Igor <igor.ptz@hotmail.com>
2021-11-23 23:23:10 -0300

    Update

---
+++ src/test/java/lab/three/client/Lab3ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.three.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
